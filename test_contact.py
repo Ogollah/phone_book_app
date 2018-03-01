2a5e79e240e9f0d352adccae7dfcca043d6afbef
Ogollah <kogollah2011@gmail.com>
2018-03-01 18:03:02 +0300

    add update function and test pass

--- test_contact.py
+++ test_contact.py
@@ -3,13 +3,12 @@ import unittest
 from contact import Contacts
 
 class TestContactsCase(unittest.TestCase):
-
+    
     # test create contact
     def test_create_contact(self):
         contact = Contacts()
         response = contact.create_contact("manu", "0712345678")
         self.assertEqual(response['message'], 'Contact created!')
-
 
         # test view contact
     def test_view_contact(self):
@@ -18,19 +17,17 @@ class TestContactsCase(unittest.TestCase):
         response = contact.view_contact(contact)
         self.assertEqual(response, {'name':"manu", 'phon_num':"0712345678"})
 
-
     def test_delete_contact(self):
         contact = Contacts()
         response = contact.delete_contact(contact)
         self.assertEqual(response['message'], 'Contact deleted!')
 
-
-
-
-
-        #test update contact
+    #test update contact
     def test_update_contact(self):
-        pass
+        contact = Contacts()
+        response = contact.update_contact(contact)
+        self.assertEqual(response['message'], 'Contact updated!')
+ 
 
 
     
